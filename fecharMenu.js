7fe40e1fd8b19a1b3f47c93584fdbc59066b836d
Felipe Buche <felipe10buche@gmail.com>
2024-08-07 23:04:43 -0300

    FINALMENTE CONSEGUI FECHAR A PAGINA DE LOGIN

--- fecharMenu.js
+++ fecharMenu.js
@@ -1,20 +1,11 @@
+var formReg = document.querySelector('.form-reg');
+var btnCadt = document.querySelector('.btnCadt');
+var fechar = document.querySelector('.fechar');
 
-document.addEventListener("DOMContentLoaded", function() {
-    var modal = document.querySelector(".form-reg"); // Seleciona o elemento com a classe .form-reg
-    var btn = document.getElementById(".btnCadt"); // Seleciona o botão de abrir o modal
-    var span = document.getElementById("close-register"); // Seleciona o botão de fechar o modal
+btnCadt.addEventListener('click', ()=>{
+    telaLogin.classList.add('ativoTela');
+});
 
-    btn.onclick = function() {
-        modal.style.display = "block"; // Mostra o modal
-    }
-
-    span.onclick = function() {
-        modal.style.display = "none"; // Esconde o modal
-    }
-
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none"; // Esconde o modal se o usuário clicar fora dele
-        }
-    }
+fechar.addEventListener('click', ()=>{
+    telaLogin.classList.remove('ativoTela');
 });
