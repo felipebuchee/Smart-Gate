bc0736d56d6b37af70be8dae5b0401477de96bb3
Felipe Buche <felipe10buche@gmail.com>
2024-08-09 15:35:48 -0300

    tive que voltar versao nessa caceta

--- fecharMenu.js
+++ fecharMenu.js
@@ -1,11 +1,12 @@
+
 var formReg = document.querySelector('.form-reg');
 var btnCadt = document.querySelector('.btnCadt');
 var fechar = document.querySelector('.fechar');
 
 btnCadt.addEventListener('click', ()=>{
-    formReg.classList.add('ativoTela');
+    telaLogin.classList.add('ativoTela');
 });
 
 fechar.addEventListener('click', ()=>{
-    formReg.classList.remove('ativoTela');
+    telaLogin.classList.remove('ativoTela');
 });
