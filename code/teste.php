720a8e9a5ebf580fc0c7b123e5e10fcd20301808
felipebuchee <felipe10buche@gmail.com>
2024-10-09 10:26:19 -0300

    Co-authored-by: Walmonn Eduardo <WalmonnEduardo@users.noreply.github.com>

---
+++ code/teste.php
@@ -0,0 +1,18 @@
+<?php
+$alunos = array();
+$alunos[] = array(
+    "nome" => "Walmonn",
+    "numeroMatricula" => "20233014661",
+    "emailResponsavel" => "enigma.kalhi.linux@gmail.com"
+);
+$alunos[] = array(
+    "nome" => "Sarah",
+    "numeroMatricula" => "20233014632",
+    "emailResponsavel" => "sarahelizabete98@gmail.com"
+);
+$alunos[] = array(
+    "nome" => "Felibe Buche",
+    "numeroMatricula" => "20233014447",
+    "emailResponsavel" => "felipe10buche@gmail.com"
+);
+?>
