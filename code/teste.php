08cbb4067d0ac7603fedda49e4d14cc96b5f375e
felipebuchee <felipe10buche@gmail.com>
2024-10-09 10:35:41 -0300

    .

--- code/teste.php
+++ code/teste.php
@@ -15,4 +15,6 @@ $alunos[] = array(
     "numeroMatricula" => "20233014447",
     "emailResponsavel" => "felipe10buche@gmail.com"
 );
+$alunos = json_encode($alunos, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+file_put_contents("./data/usuarios.json",$alunos);
 ?>
