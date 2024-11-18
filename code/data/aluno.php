a68f9fe562855a33be1cf125cd8a4f4119e3c616
Walmonn Eduardo <130590544+WalmonnEduardo@users.noreply.github.com>
2024-11-18 07:25:08 -0300

    Create aluno.php

---
+++ code/data/aluno.php
@@ -0,0 +1,10 @@
+[
+    {
+        "nome": "Aura",
+        "numeroMatricula": 159753456,
+        "cpf": "87187178",
+        "senha": 22072000,
+        "email": "teste@hotmail.com",
+        "tag": "4D 5E 6F"
+    }
+]
